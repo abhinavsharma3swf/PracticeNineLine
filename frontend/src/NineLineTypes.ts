1caa9ba1a193f3f8c682e3496be1e8c5cfeb65e5
Abhinav Sharma <abhinav.sharma3@hotmailcom>
2025-06-02 14:18:06 -0500

    updated Types to include SoftDelete Field

--- frontend/src/NineLineTypes.ts
+++ frontend/src/NineLineTypes.ts
@@ -4,5 +4,6 @@ export type NineLineTypes= {
     line2: string,
     line3: string,
     line4: string,
-    line5: string
+    line5: string,
+    softDelete?: boolean //if true the item should be hidden
 }
